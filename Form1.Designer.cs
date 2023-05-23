9b7013c392240a8ae9602bb9ebd8355cd8f8649a
Piotr Hinc <piotr.hinc4@gmail.com>
2023-05-23 22:39:42 +0200

    Preparation of the form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,395 @@ namespace CarGame
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.auta1 = new System.Windows.Forms.Label();
+            this.lblNumer1 = new System.Windows.Forms.Label();
+            this.lblNumer2 = new System.Windows.Forms.Label();
+            this.lblNumer3 = new System.Windows.Forms.Label();
+            this.lblNumer4 = new System.Windows.Forms.Label();
+            this.lblNumer5 = new System.Windows.Forms.Label();
+            this.lblNazwa1 = new System.Windows.Forms.Label();
+            this.lblNazwa2 = new System.Windows.Forms.Label();
+            this.lblNazwa3 = new System.Windows.Forms.Label();
+            this.lblNazwa4 = new System.Windows.Forms.Label();
+            this.lblNazwa5 = new System.Windows.Forms.Label();
+            this.lblWartosc1 = new System.Windows.Forms.Label();
+            this.lblWartosc2 = new System.Windows.Forms.Label();
+            this.lblWartosc3 = new System.Windows.Forms.Label();
+            this.lblWartosc4 = new System.Windows.Forms.Label();
+            this.lblWartosc5 = new System.Windows.Forms.Label();
+            this.lblUszkodzenia1 = new System.Windows.Forms.Label();
+            this.lblUszkodzenia2 = new System.Windows.Forms.Label();
+            this.lblUszkodzenia3 = new System.Windows.Forms.Label();
+            this.lblUszkodzenia4 = new System.Windows.Forms.Label();
+            this.lblUszkodzenia5 = new System.Windows.Forms.Label();
+            this.lblWartoscRynkowa1 = new System.Windows.Forms.Label();
+            this.lblWartoscRynkowa2 = new System.Windows.Forms.Label();
+            this.lblWartoscRynkowa3 = new System.Windows.Forms.Label();
+            this.lblWartoscRynkowa4 = new System.Windows.Forms.Label();
+            this.lblWartoscRynkowa5 = new System.Windows.Forms.Label();
+            this.auta2 = new System.Windows.Forms.Label();
+            this.lblBudzet = new System.Windows.Forms.Label();
+            this.lblTura = new System.Windows.Forms.Label();
+            this.bttnKupno = new System.Windows.Forms.Button();
+            this.lblInformacja = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // auta1
+            // 
+            this.auta1.AutoSize = true;
+            this.auta1.Location = new System.Drawing.Point(49, 0);
+            this.auta1.Name = "auta1";
+            this.auta1.Size = new System.Drawing.Size(155, 17);
+            this.auta1.TabIndex = 0;
+            this.auta1.Text = "Posiadane samochody:";
+            // 
+            // lblNumer1
+            // 
+            this.lblNumer1.AutoSize = true;
+            this.lblNumer1.Location = new System.Drawing.Point(49, 57);
+            this.lblNumer1.Name = "lblNumer1";
+            this.lblNumer1.Size = new System.Drawing.Size(20, 17);
+            this.lblNumer1.TabIndex = 1;
+            this.lblNumer1.Text = "1.";
+            // 
+            // lblNumer2
+            // 
+            this.lblNumer2.AutoSize = true;
+            this.lblNumer2.Location = new System.Drawing.Point(49, 89);
+            this.lblNumer2.Name = "lblNumer2";
+            this.lblNumer2.Size = new System.Drawing.Size(20, 17);
+            this.lblNumer2.TabIndex = 2;
+            this.lblNumer2.Text = "2.";
+            // 
+            // lblNumer3
+            // 
+            this.lblNumer3.AutoSize = true;
+            this.lblNumer3.Location = new System.Drawing.Point(49, 121);
+            this.lblNumer3.Name = "lblNumer3";
+            this.lblNumer3.Size = new System.Drawing.Size(20, 17);
+            this.lblNumer3.TabIndex = 3;
+            this.lblNumer3.Text = "3.";
+            // 
+            // lblNumer4
+            // 
+            this.lblNumer4.AutoSize = true;
+            this.lblNumer4.Location = new System.Drawing.Point(49, 155);
+            this.lblNumer4.Name = "lblNumer4";
+            this.lblNumer4.Size = new System.Drawing.Size(20, 17);
+            this.lblNumer4.TabIndex = 4;
+            this.lblNumer4.Text = "4.";
+            // 
+            // lblNumer5
+            // 
+            this.lblNumer5.AutoSize = true;
+            this.lblNumer5.Location = new System.Drawing.Point(49, 183);
+            this.lblNumer5.Name = "lblNumer5";
+            this.lblNumer5.Size = new System.Drawing.Size(20, 17);
+            this.lblNumer5.TabIndex = 5;
+            this.lblNumer5.Text = "5.";
+            // 
+            // lblNazwa1
+            // 
+            this.lblNazwa1.AutoSize = true;
+            this.lblNazwa1.Location = new System.Drawing.Point(104, 57);
+            this.lblNazwa1.Name = "lblNazwa1";
+            this.lblNazwa1.Size = new System.Drawing.Size(23, 17);
+            this.lblNazwa1.TabIndex = 6;
+            this.lblNazwa1.Text = "---";
+            // 
+            // lblNazwa2
+            // 
+            this.lblNazwa2.AutoSize = true;
+            this.lblNazwa2.Location = new System.Drawing.Point(104, 89);
+            this.lblNazwa2.Name = "lblNazwa2";
+            this.lblNazwa2.Size = new System.Drawing.Size(23, 17);
+            this.lblNazwa2.TabIndex = 7;
+            this.lblNazwa2.Text = "---";
+            // 
+            // lblNazwa3
+            // 
+            this.lblNazwa3.AutoSize = true;
+            this.lblNazwa3.Location = new System.Drawing.Point(104, 121);
+            this.lblNazwa3.Name = "lblNazwa3";
+            this.lblNazwa3.Size = new System.Drawing.Size(23, 17);
+            this.lblNazwa3.TabIndex = 8;
+            this.lblNazwa3.Text = "---";
+            // 
+            // lblNazwa4
+            // 
+            this.lblNazwa4.AutoSize = true;
+            this.lblNazwa4.Location = new System.Drawing.Point(104, 154);
+            this.lblNazwa4.Name = "lblNazwa4";
+            this.lblNazwa4.Size = new System.Drawing.Size(23, 17);
+            this.lblNazwa4.TabIndex = 9;
+            this.lblNazwa4.Text = "---";
+            // 
+            // lblNazwa5
+            // 
+            this.lblNazwa5.AutoSize = true;
+            this.lblNazwa5.Location = new System.Drawing.Point(104, 183);
+            this.lblNazwa5.Name = "lblNazwa5";
+            this.lblNazwa5.Size = new System.Drawing.Size(23, 17);
+            this.lblNazwa5.TabIndex = 10;
+            this.lblNazwa5.Text = "---";
+            // 
+            // lblWartosc1
+            // 
+            this.lblWartosc1.AutoSize = true;
+            this.lblWartosc1.Location = new System.Drawing.Point(221, 57);
+            this.lblWartosc1.Name = "lblWartosc1";
+            this.lblWartosc1.Size = new System.Drawing.Size(23, 17);
+            this.lblWartosc1.TabIndex = 11;
+            this.lblWartosc1.Text = "---";
+            // 
+            // lblWartosc2
+            // 
+            this.lblWartosc2.AutoSize = true;
+            this.lblWartosc2.Location = new System.Drawing.Point(221, 89);
+            this.lblWartosc2.Name = "lblWartosc2";
+            this.lblWartosc2.Size = new System.Drawing.Size(23, 17);
+            this.lblWartosc2.TabIndex = 12;
+            this.lblWartosc2.Text = "---";
+            // 
+            // lblWartosc3
+            // 
+            this.lblWartosc3.AutoSize = true;
+            this.lblWartosc3.Location = new System.Drawing.Point(221, 121);
+            this.lblWartosc3.Name = "lblWartosc3";
+            this.lblWartosc3.Size = new System.Drawing.Size(23, 17);
+            this.lblWartosc3.TabIndex = 13;
+            this.lblWartosc3.Text = "---";
+            // 
+            // lblWartosc4
+            // 
+            this.lblWartosc4.AutoSize = true;
+            this.lblWartosc4.Location = new System.Drawing.Point(221, 155);
+            this.lblWartosc4.Name = "lblWartosc4";
+            this.lblWartosc4.Size = new System.Drawing.Size(23, 17);
+            this.lblWartosc4.TabIndex = 14;
+            this.lblWartosc4.Text = "---";
+            // 
+            // lblWartosc5
+            // 
+            this.lblWartosc5.AutoSize = true;
+            this.lblWartosc5.Location = new System.Drawing.Point(221, 183);
+            this.lblWartosc5.Name = "lblWartosc5";
+            this.lblWartosc5.Size = new System.Drawing.Size(23, 17);
+            this.lblWartosc5.TabIndex = 15;
+            this.lblWartosc5.Text = "---";
+            // 
+            // lblUszkodzenia1
+            // 
+            this.lblUszkodzenia1.AutoSize = true;
+            this.lblUszkodzenia1.Location = new System.Drawing.Point(324, 57);
+            this.lblUszkodzenia1.Name = "lblUszkodzenia1";
+            this.lblUszkodzenia1.Size = new System.Drawing.Size(23, 17);
+            this.lblUszkodzenia1.TabIndex = 16;
+            this.lblUszkodzenia1.Text = "---";
+            // 
+            // lblUszkodzenia2
+            // 
+            this.lblUszkodzenia2.AutoSize = true;
+            this.lblUszkodzenia2.Location = new System.Drawing.Point(324, 89);
+            this.lblUszkodzenia2.Name = "lblUszkodzenia2";
+            this.lblUszkodzenia2.Size = new System.Drawing.Size(23, 17);
+            this.lblUszkodzenia2.TabIndex = 17;
+            this.lblUszkodzenia2.Text = "---";
+            // 
+            // lblUszkodzenia3
+            // 
+            this.lblUszkodzenia3.AutoSize = true;
+            this.lblUszkodzenia3.Location = new System.Drawing.Point(324, 121);
+            this.lblUszkodzenia3.Name = "lblUszkodzenia3";
+            this.lblUszkodzenia3.Size = new System.Drawing.Size(23, 17);
+            this.lblUszkodzenia3.TabIndex = 18;
+            this.lblUszkodzenia3.Text = "---";
+            // 
+            // lblUszkodzenia4
+            // 
+            this.lblUszkodzenia4.AutoSize = true;
+            this.lblUszkodzenia4.Location = new System.Drawing.Point(324, 154);
+            this.lblUszkodzenia4.Name = "lblUszkodzenia4";
+            this.lblUszkodzenia4.Size = new System.Drawing.Size(23, 17);
+            this.lblUszkodzenia4.TabIndex = 19;
+            this.lblUszkodzenia4.Text = "---";
+            // 
+            // lblUszkodzenia5
+            // 
+            this.lblUszkodzenia5.AutoSize = true;
+            this.lblUszkodzenia5.Location = new System.Drawing.Point(324, 182);
+            this.lblUszkodzenia5.Name = "lblUszkodzenia5";
+            this.lblUszkodzenia5.Size = new System.Drawing.Size(23, 17);
+            this.lblUszkodzenia5.TabIndex = 20;
+            this.lblUszkodzenia5.Text = "---";
+            // 
+            // lblWartoscRynkowa1
+            // 
+            this.lblWartoscRynkowa1.AutoSize = true;
+            this.lblWartoscRynkowa1.Location = new System.Drawing.Point(408, 57);
+            this.lblWartoscRynkowa1.Name = "lblWartoscRynkowa1";
+            this.lblWartoscRynkowa1.Size = new System.Drawing.Size(23, 17);
+            this.lblWartoscRynkowa1.TabIndex = 21;
+            this.lblWartoscRynkowa1.Text = "---";
+            // 
+            // lblWartoscRynkowa2
+            // 
+            this.lblWartoscRynkowa2.AutoSize = true;
+            this.lblWartoscRynkowa2.Location = new System.Drawing.Point(408, 89);
+            this.lblWartoscRynkowa2.Name = "lblWartoscRynkowa2";
+            this.lblWartoscRynkowa2.Size = new System.Drawing.Size(23, 17);
+            this.lblWartoscRynkowa2.TabIndex = 22;
+            this.lblWartoscRynkowa2.Text = "---";
+            // 
+            // lblWartoscRynkowa3
+            // 
+            this.lblWartoscRynkowa3.AutoSize = true;
+            this.lblWartoscRynkowa3.Location = new System.Drawing.Point(408, 121);
+            this.lblWartoscRynkowa3.Name = "lblWartoscRynkowa3";
+            this.lblWartoscRynkowa3.Size = new System.Drawing.Size(23, 17);
+            this.lblWartoscRynkowa3.TabIndex = 23;
+            this.lblWartoscRynkowa3.Text = "---";
+            // 
+            // lblWartoscRynkowa4
+            // 
+            this.lblWartoscRynkowa4.AutoSize = true;
+            this.lblWartoscRynkowa4.Location = new System.Drawing.Point(408, 154);
+            this.lblWartoscRynkowa4.Name = "lblWartoscRynkowa4";
+            this.lblWartoscRynkowa4.Size = new System.Drawing.Size(23, 17);
+            this.lblWartoscRynkowa4.TabIndex = 24;
+            this.lblWartoscRynkowa4.Text = "---";
+            // 
+            // lblWartoscRynkowa5
+            // 
+            this.lblWartoscRynkowa5.AutoSize = true;
+            this.lblWartoscRynkowa5.Location = new System.Drawing.Point(408, 183);
+            this.lblWartoscRynkowa5.Name = "lblWartoscRynkowa5";
+            this.lblWartoscRynkowa5.Size = new System.Drawing.Size(23, 17);
+            this.lblWartoscRynkowa5.TabIndex = 25;
+            this.lblWartoscRynkowa5.Text = "---";
+            // 
+            // auta2
+            // 
+            this.auta2.AutoSize = true;
+            this.auta2.Location = new System.Drawing.Point(49, 28);
+            this.auta2.Name = "auta2";
+            this.auta2.Size = new System.Drawing.Size(455, 17);
+            this.auta2.TabIndex = 26;
+            this.auta2.Text = "Lp.  |       Nazwa       |       Wartość       | Uszkodzenia | Wartość rynkowa";
+            // 
+            // lblBudzet
+            // 
+            this.lblBudzet.AutoSize = true;
+            this.lblBudzet.Location = new System.Drawing.Point(634, 28);
+            this.lblBudzet.Name = "lblBudzet";
+            this.lblBudzet.Size = new System.Drawing.Size(100, 17);
+            this.lblBudzet.TabIndex = 27;
+            this.lblBudzet.Text = "Budżet: 20000";
+            // 
+            // lblTura
+            // 
+            this.lblTura.AutoSize = true;
+            this.lblTura.Location = new System.Drawing.Point(634, 57);
+            this.lblTura.Name = "lblTura";
+            this.lblTura.Size = new System.Drawing.Size(54, 17);
+            this.lblTura.TabIndex = 28;
+            this.lblTura.Text = "Tura: 1";
+            // 
+            // bttnKupno
+            // 
+            this.bttnKupno.Location = new System.Drawing.Point(52, 246);
+            this.bttnKupno.Name = "bttnKupno";
+            this.bttnKupno.Size = new System.Drawing.Size(130, 32);
+            this.bttnKupno.TabIndex = 29;
+            this.bttnKupno.Text = "Kup samochód";
+            this.bttnKupno.UseVisualStyleBackColor = true;
+            this.bttnKupno.Click += new System.EventHandler(this.bttnKupno_Click);
+            // 
+            // lblInformacja
+            // 
+            this.lblInformacja.AutoSize = true;
+            this.lblInformacja.Location = new System.Drawing.Point(49, 293);
+            this.lblInformacja.Name = "lblInformacja";
+            this.lblInformacja.Size = new System.Drawing.Size(109, 17);
+            this.lblInformacja.TabIndex = 30;
+            this.lblInformacja.Text = "Informacje: brak";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 335);
+            this.Controls.Add(this.lblInformacja);
+            this.Controls.Add(this.bttnKupno);
+            this.Controls.Add(this.lblTura);
+            this.Controls.Add(this.lblBudzet);
+            this.Controls.Add(this.auta2);
+            this.Controls.Add(this.lblWartoscRynkowa5);
+            this.Controls.Add(this.lblWartoscRynkowa4);
+            this.Controls.Add(this.lblWartoscRynkowa3);
+            this.Controls.Add(this.lblWartoscRynkowa2);
+            this.Controls.Add(this.lblWartoscRynkowa1);
+            this.Controls.Add(this.lblUszkodzenia5);
+            this.Controls.Add(this.lblUszkodzenia4);
+            this.Controls.Add(this.lblUszkodzenia3);
+            this.Controls.Add(this.lblUszkodzenia2);
+            this.Controls.Add(this.lblUszkodzenia1);
+            this.Controls.Add(this.lblWartosc5);
+            this.Controls.Add(this.lblWartosc4);
+            this.Controls.Add(this.lblWartosc3);
+            this.Controls.Add(this.lblWartosc2);
+            this.Controls.Add(this.lblWartosc1);
+            this.Controls.Add(this.lblNazwa5);
+            this.Controls.Add(this.lblNazwa4);
+            this.Controls.Add(this.lblNazwa3);
+            this.Controls.Add(this.lblNazwa2);
+            this.Controls.Add(this.lblNazwa1);
+            this.Controls.Add(this.lblNumer5);
+            this.Controls.Add(this.lblNumer4);
+            this.Controls.Add(this.lblNumer3);
+            this.Controls.Add(this.lblNumer2);
+            this.Controls.Add(this.lblNumer1);
+            this.Controls.Add(this.auta1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label auta1;
+        private System.Windows.Forms.Label lblNumer1;
+        private System.Windows.Forms.Label lblNumer2;
+        private System.Windows.Forms.Label lblNumer3;
+        private System.Windows.Forms.Label lblNumer4;
+        private System.Windows.Forms.Label lblNumer5;
+        private System.Windows.Forms.Label lblNazwa1;
+        private System.Windows.Forms.Label lblNazwa2;
+        private System.Windows.Forms.Label lblNazwa3;
+        private System.Windows.Forms.Label lblNazwa4;
+        private System.Windows.Forms.Label lblNazwa5;
+        private System.Windows.Forms.Label lblWartosc1;
+        private System.Windows.Forms.Label lblWartosc2;
+        private System.Windows.Forms.Label lblWartosc3;
+        private System.Windows.Forms.Label lblWartosc4;
+        private System.Windows.Forms.Label lblWartosc5;
+        private System.Windows.Forms.Label lblUszkodzenia1;
+        private System.Windows.Forms.Label lblUszkodzenia2;
+        private System.Windows.Forms.Label lblUszkodzenia3;
+        private System.Windows.Forms.Label lblUszkodzenia4;
+        private System.Windows.Forms.Label lblUszkodzenia5;
+        private System.Windows.Forms.Label lblWartoscRynkowa1;
+        private System.Windows.Forms.Label lblWartoscRynkowa2;
+        private System.Windows.Forms.Label lblWartoscRynkowa3;
+        private System.Windows.Forms.Label lblWartoscRynkowa4;
+        private System.Windows.Forms.Label lblWartoscRynkowa5;
+        private System.Windows.Forms.Label auta2;
+        private System.Windows.Forms.Label lblBudzet;
+        private System.Windows.Forms.Label lblTura;
+        private System.Windows.Forms.Button bttnKupno;
+        private System.Windows.Forms.Label lblInformacja;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,71 @@ namespace CarGame
 {
     public partial class Form1 : Form
     {
+        private static readonly int DEFAULT_TURA=1;
+        private static readonly int DEFAULT_BUDZET=20000;
+        private static readonly int DEFAULT_ILOSC_SAMOCHODOW=0;
+        public int tura = DEFAULT_TURA;
+        public int budzet = DEFAULT_BUDZET;
+        public int iloscSamochodow = DEFAULT_ILOSC_SAMOCHODOW;
+        public string[,] samochody = new string[5, 4];
         public Form1()
         {
             InitializeComponent();
+        }
+        private void aktualizacjaDanych()
+        {
+            lblBudzet.Text = "Budżet: " + budzet;
+            lblTura.Text = "Tura: " + tura;
+        }
+        string czyJestInformacja(string informacja)
+        {
+            if (informacja == null)
+            {
+                return "---";
+            }
+            else
+            {
+                return informacja;
+            }
+        }
+        private void aktualizacjaTabeli()
+        {
+            lblNazwa1.Text = czyJestInformacja(samochody[0,0]);
+            lblWartosc1.Text = czyJestInformacja(samochody[0, 1]);
+            lblUszkodzenia1.Text = czyJestInformacja(samochody[0, 2]);
+            lblWartoscRynkowa1.Text = czyJestInformacja(samochody[0, 3]);
+
+            lblNazwa2.Text = czyJestInformacja(samochody[1, 0]);
+            lblWartosc2.Text = czyJestInformacja(samochody[1, 1]);
+            lblUszkodzenia2.Text = czyJestInformacja(samochody[1, 2]);
+            lblWartoscRynkowa2.Text = czyJestInformacja(samochody[1, 3]);
+
+            lblNazwa3.Text = czyJestInformacja(samochody[2, 0]);
+            lblWartosc3.Text = czyJestInformacja(samochody[2, 1]);
+            lblUszkodzenia3.Text = czyJestInformacja(samochody[2, 2]);
+            lblWartoscRynkowa3.Text = czyJestInformacja(samochody[2, 3]);
+
+            lblNazwa4.Text = czyJestInformacja(samochody[3, 0]);
+            lblWartosc4.Text = czyJestInformacja(samochody[3, 1]);
+            lblUszkodzenia4.Text = czyJestInformacja(samochody[3, 2]);
+            lblWartoscRynkowa4.Text = czyJestInformacja(samochody[3, 3]);
+
+            lblNazwa5.Text = czyJestInformacja(samochody[4, 0]);
+            lblWartosc5.Text = czyJestInformacja(samochody[4, 1]);
+            lblUszkodzenia5.Text = czyJestInformacja(samochody[4, 2]);
+            lblWartoscRynkowa5.Text = czyJestInformacja(samochody[4, 3]);
+        }
+
+        private void bttnKupno_Click(object sender, EventArgs e)
+        {
+            if (iloscSamochodow >= 5)
+            {
+                lblInformacja.Text = "Informacje: Zbyt mało miejsca w garażu, aby kupić nowy samochód!!!!";
+            }
+            else
+            { 
+                
+            }
         }
     }
 }
